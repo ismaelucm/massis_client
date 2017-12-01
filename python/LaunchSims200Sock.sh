cf7732a896b64996f09cc091e4384441cb6af0df
ismaelucm <isagredo@ucm.es>
2017-12-01 15:15:59 +0100

    Moving files to root

--- python/LaunchSims200Sock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3.5 plot_sensors.py -t 1 -p 8085 -c 3&
-sleep 1s
-python3.5 client_p_v1.0.6_read_from_file.py -s scene.json -f ./Sim200/sim200.json -d A -c red -n sim0 -i 127.0.0.1 -p 8085&
-sleep 1s
-python3.5 client_p_v1.0.6_read_from_file.py -s scene.json -f ./Sim200/sim200.json -d B -c green -n sim1 -i 127.0.0.1 -p 8086&
-sleep 1s
-python3.5 client_p_v1.0.6_read_from_file.py -s scene.json -f ./Sim200/sim200.json -d C -c blue -n sim2 -i 127.0.0.1 -p 8087&
-
--- python/Readme_sim400.txt
+++
@@ -1,5 +0,0 @@
-The simulation 400 try to simulate the entrance to class in the early morning
-
-We can launch the simulation, running the following command:
-
-./LauchSims400.sh
